a71d39e0a482b65edd007f10c3d8cebcc1dab0a9
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-02-26 15:01:16 -0500

    Update NoAccess.js

--- src/Pages/Home/NoAccess.js
+++ src/Pages/Home/NoAccess.js
@@ -20,6 +20,8 @@ export default function NoAccess({expiredStatus}) {
     // Constants // 
     ///////////////
 
+    const [COLORS, setColors] = useRecoilState(colorState)
+    const FONTS = useRecoilValue(fontState)
 
     /////////////////
     // Local State //
@@ -31,12 +33,48 @@ export default function NoAccess({expiredStatus}) {
     // Recoil State //
     //////////////////
 
+        const [user, setUser] = useRecoilState(userState)
 
-//////////////////////
+///////////////////////
 ///                 ///
 ///    Renderings   ///
 ///                 ///
 ///////////////////////
+
+    function MAIN(){
+        return(
+            <View style={{justifyContent: 'center', alignItems: 'center', marginRight: 20, marginLeft: 20}}>
+                <Text style={{color: COLORS.iconLight, ...FONTS.Title}}>
+                    Your Trial has expired!
+                </Text>
+                {renderPrompt()}
+            </View>
+        )
+    }
+
+    function renderPrompt(){
+        if (expiredStatus === "expiredOwner"){
+            return(
+                <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
+                    <Text style={{...FONTS.SubTitle, color: COLORS.iconLight, fontSize: 22, textAlign: 'center'}}>
+                        Your organization's trial period has ended!
+                    </Text>
+                    <Text style={{fontFamily: 'Gilroy-Medium', color: COLORS.iconLight, marginTop: 20, fontSize: 18, textAlign: 'center'}}>
+                        Please navigate to the Online Web Portal and add payment information to continue with your Kidz-N-Motion experience!
+                    </Text>
+                    <Text style={{justifyContent: 'center', alignItems: 'center', fontFamily: 'Gilroy-Medium', color: COLORS.iconLight, marginTop: 20, fontSize: 18, width: 300, textAlign: 'center'}}>
+                        If you are the Administrator of this account, please go to 
+                    </Text>
+                    <Text style={{marginTop: 10, fontFamily: 'Gilroy-Bold', fontSize: 20}}>
+                        https://kids-in-motion.vercel.app
+                    </Text>
+                    <Text style={{justifyContent: 'center', alignItems: 'center', fontFamily: 'Gilroy-Medium', color: COLORS.iconLight, marginTop: 20, fontSize: 18, width: 300, textAlign: 'center'}}>
+                        And enter in your payment information to continue
+                    </Text>
+                </View>
+            )
+        }
+    }
 
 
 ///////////////////////
@@ -51,9 +89,5 @@ export default function NoAccess({expiredStatus}) {
 ///    Main Render  ///
 ///                 ///
 ///////////////////////
-    return(
-       <View>
-        
-       </View>
-    )
+    return MAIN()
 }
