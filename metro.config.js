740daaa5a7fcc4193e5162d4845ce84dc9200c88
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-11-02 09:31:55 -0400

    Followed upgrade instructions rn 0.71 -> rn 0.72

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,15 @@
  *
  * @format
  */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
